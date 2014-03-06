298d53eb087cf1234f5596fc6ec5176e38cb2ff9
Chris Morris <cmorris@ccistudios.com>
2014-03-06 08:53:37 -0500

    Added drush alias for live site

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -10,3 +10,13 @@ $aliases['dev'] = array(
 	),
 	'ssh-options'=> '-p 37241'
 );
+
+$aliases['live'] = array(
+	'uri'=> 'live.magermansandraes.com',
+	'root' => '/home/magraes/subdomains/live/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'magraes',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
